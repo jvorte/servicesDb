ed72cd44db95bbe6675eac1f3878c9a8c2928f28
Dimitris <jvortelinas@gmail.com>
2025-08-28 14:20:55 +0200

    start οκ

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="icon" href="storage/images/tools.png" type="engine/png">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
