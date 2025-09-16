04d93549c9a2d91ec89db1d92489f627f0333238
Dimitris <jvortelinas@gmail.com>
2025-09-16 15:46:34 +0200

    ok

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="flex items-center gap-2 font-semibold text-xl text-gray-800 leading-tight">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-gauge-icon lucide-circle-gauge">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-gauge-icon lucide-circle-gauge">
                 <path d="M15.6 2.7a10 10 0 1 0 5.7 5.7" />
                 <circle cx="12" cy="12" r="2" />
                 <path d="M13.4 10.6 19 5" />
@@ -67,7 +67,7 @@
 
         <!-- Recent Services -->
         <div class="bg-white shadow rounded-lg p-4">
-            <h3 class="font-semibold mb-3">Recent Services</h3>
+            <h3 class="font-semibold mb-3 text-xl">Recent Services</h3>
             @foreach($vehicles as $vehicle)
                 <div class="mb-4">
                     <h4 class="font-medium">{{ $vehicle->brand }} {{ $vehicle->model }} ({{ $vehicle->type }})</h4>
